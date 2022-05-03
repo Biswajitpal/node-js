20a3caeec7ec6a766d4539ba3c1187e829e911dd
Biswajit <biswajit@rimsys.io>
2022-05-03 15:14:49 +0530

    added promise function

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 let a=20;
 let b=10;
-
+/*
 setTimeout(()=>{
    b=30;
    console.log("sync",a+b);
-},2000)
-
-
+},2000)*/
+let waitingData = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve(30);
+     },2000)
+})
+waitingData.then((data) => {
+    b = data;
+    console.log(a+b);
+})
 console.log("async",a+b)
