4f66e81b0274c365ee7ff502a2104a598be62a02
Biswajit <biswajit@rimsys.io>
2022-05-03 09:01:47 +0530

    add export module

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const app=require('./app')
+const app=require('./app');
+const exModul = require('./exportModule');
 let arr=[4,1,6,3,4,89,0];
 console.log(app.xyz())
 const result =arr.filter((item)=>{
     return item>=4
 });
 console.warn(result);
+console.warn(exModul.a);
+let v = 4;
+let newarr = arr.filter((i,v)=>{
+    return i>=5;
+});
+console.log(newarr);
